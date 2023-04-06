f1c0e3d5c0d7926b3833e6895e99f7704b092090
GiselleMR <giselle.rubin1@gmail.com>
2023-04-06 14:24:00 -0400

    add button to add new note

--- src/pages/NewNotePage/NewNotePage.jsx
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 export default function NewNotePage() {
+  const [note, setNote] = useState('');
+
   return (
-    <h1>NewNotePage</h1>
+    <>
+      <h1>New Note</h1>
+      <form action="">
+        <label htmlFor="text">Text:
+          <input type="text" name="text" id="text" />
+        </label>
+        <input type="submit" value="add note" />
+      </form>
+    </>
   );
 }
