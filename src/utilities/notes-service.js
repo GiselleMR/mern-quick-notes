ecfe6e4603b58279d9b53db5d17772bb53ad371a
GiselleMR <giselle.rubin1@gmail.com>
2023-04-06 15:09:45 -0400

    create service function and api function

---
+++ src/utilities/notes-service.js
@@ -0,0 +1,9 @@
+import * as notesAPT from './notes-api';
+
+export async function createNote(noteData) {
+    // const token = await usersAPI.signUp(userData);
+    // localStorage.setItem('token', token);
+    // return getUser();
+    notesAPI.createNoteRequest(noteDate)
+  }
+  
